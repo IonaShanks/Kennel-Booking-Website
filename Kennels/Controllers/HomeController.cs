ffe46239af1e88d0e2d6c0132a6e09de1be84ece
Iona <Ionashanks@outlook.com>
2017-04-30 16:44:22 +0100

    Added a contact form with email

--- Kennels/Controllers/HomeController.cs
+++ Kennels/Controllers/HomeController.cs
@@ -4,74 +4,16 @@ using System.Web.Mvc;
 using Kennels.Models;
 using System;
 using System.Collections.Generic;
+using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Kennels.Controllers
 {
     public class HomeController : Controller
     {
-        private KennelsContext db = new KennelsContext();
+        private KennelsContext db = new KennelsContext(); 
        
-        //public ActionResult Index(string county, string searchName, DateTime? searchStart, DateTime? searchEnd)
-        //{
-        //    //Creates a list of distinct countys that have kennels in them.
-        //    var CountyList = new List<string>();
-        //    var CountyQry = db.Kennel.Select(c => c.County.ToString());
-        //    CountyList.AddRange(CountyQry.Distinct());
-        //    ViewBag.county = new SelectList(CountyList);
-
-        //    IQueryable<Kennel> kennels = db.Kennel;
-
-        //    if (searchStart.HasValue && searchEnd.HasValue)
-        //    {
-        //        //Converts the variables from DateTime? to DateTime 
-        //        DateTime ss = Convert.ToDateTime(searchStart);
-        //        DateTime se = Convert.ToDateTime(searchEnd);
-
-        //        //Creates a list of the kennels to be displayed
-        //        var avList = new List<Kennel>();
-        //        //Bool to keep track if a kennel is full or not
-        //        bool kenFull = false;
-        //        //Loops through every kennel in the kennel database
-        //        foreach (Kennel kennel in db.Kennel)
-        //        {
-        //            for (DateTime date = ss; date <= se; date = date.AddDays(1))
-        //            {
-        //                var ka = db.KennelAvailability.Where(k => k.KennelID == kennel.KennelID && k.BookingDate == date).FirstOrDefault();
-        //                //Could be null if the kennel hasn't been booked at all on the day
-        //                if (ka != null)
-        //                {
-        //                    //Triggers true if the kennel is full for part of or all of the search dates
-        //                    if (ka.Full == true)
-        //                    {
-        //                        kenFull = true;
-        //                    }
-        //                }
-        //            }
-        //            //If the kennel is not full for any of the days it is added to the list
-        //            if (kenFull != true)
-        //            {
-        //                avList.Add(kennel);
-        //            }
-        //        }
-        //        kennels = avList.AsQueryable();
-        //    }
-
-        //    if (!string.IsNullOrEmpty(searchName))
-        //    {
-        //        //Adds the kennels to the list that include the name searched
-        //        kennels = kennels.Where(n => n.Name.Contains(searchName));
-        //    }
-
-        //    if (!string.IsNullOrEmpty(county))
-        //    {
-        //        //Adds the kennels to the list that are in the selected county
-        //        kennels = kennels.Where(c => c.County.ToString() == county);
-        //    }
-
-        //    //Displays the view from all the queries
-        //    return View(kennels);
-        //}
-
         public ActionResult About()
         {
             ViewBag.Message = "Helping you find the ideal kennel.";
@@ -79,10 +21,62 @@ namespace Kennels.Controllers
             return View();
         }
 
+
+        public void ContactEmail(string Message, string Email, String Name)
+        {
+            //Making email            
+            var message = new MailMessage();
+            message.To.Add(new MailAddress("IonaKennel@gmail.com"));
+            message.From = new MailAddress(Email);
+            message.Subject = "Contact Form";
+            message.Body = string.Format("<p>From: " + Name + "</p><p>Email: " + Email + "</p><p>Message: " + Message + "</p>");
+            message.IsBodyHtml = true;
+
+            //Sending email
+            using (var smtp = new SmtpClient("smtp.gmail.com", 587))
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "IonaKennel@gmail.com",
+                    Password = "Tallaght1"
+                };
+                smtp.Credentials = credential;
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+            }
+        }
+        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact IS Kennel Finder";
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel vm)
+        {
+            ViewBag.Message = "Contact IS Kennel Finder";
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String Message = vm.Message;
+                    String Email = vm.EmailFrom;
+                    String Name = vm.Name;
+                    ContactEmail(Message, Email, Name);                 
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();                    
+                    TempData["Error"] = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "ModelState not valid");
+                return View(vm);
+            }
             return View();
         }
     }
---
+++ Kennels/Models/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kennels.Models
+{
+    public class ContactViewModel
+    {
+        [Required, StringLength(30, MinimumLength = 4, ErrorMessage = "Must be between 4 and 30 characters.")]
+        public string Name { get; set; }
+        [Required, EmailAddress, Display(Name="Email")]
+        public string EmailFrom { get; set; }        
+        [Required]
+        public string Message { get; set; }
+    }
+}
